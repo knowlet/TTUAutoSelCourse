5b193330b9dd13c521b90a1fb5e7556dd5559ae7
knowlet <knowlet3389@gmail.com>
2016-01-21 15:46:39 +0800

    Improve regux and Add all done confirm

--- stucis.sh
+++ stucis.sh
@@ -26,16 +26,18 @@ while [[ $(($(date "+%s")-$startTime)) -lt $runTime ]]; do
     curl -sA "$UA" "$domain/menu/seltop.php" -b ./Cookie.txt > /dev/null
     selDeny=$(curl -sA "$UA" "$domain/selcourse/ListClassCourse.php" -b ./Cookie.txt | grep 'DoAddDelSbj' | wc -l)
     if [ $selDeny -ge 1 ]; then
+        count=0; chose=0;
         while read sbj; do
-            sbj=$(echo $sbj | grep -o "^[A-Z]\w\+")
-            if [[ $sbj =~ ^[A-Z] ]]; then
-                echo "Sbj $sbj processing..."
-                result=$(curl -L -sA "$UA" --referer "$domain/selcourse/ListClassCourse.php" "$domain/selcourse/DoAddDelSbj.php?AddSbjNo=$sbj" -b ./Cookie.txt | iconv -f big5 -t utf8)
+            if [[ $sbj =~ ^[A-Z][0-9A-Z]{4,5} ]]; then
+                echo "Sbj ${BASH_REMATCH[0]} processing..."
+                result=$(curl -L -sA "$UA" --referer "$domain/selcourse/ListClassCourse.php" "$domain/selcourse/DoAddDelSbj.php?AddSbjNo=${BASH_REMATCH[0]}" -b ./Cookie.txt | iconv -f big5 -t utf8)
                 msg=$(echo $result | grep -o "alert(.*);" | sed "s/[alert('');]//g")
                 echo $msg
+                if [ -n $msg ]; then ((chose++)); fi
+                ((count++))
             fi
         done < sbj.txt
-        echo "done, wait 3 sec.."
+        if [ $chose -eq $count ]; then echo "All done."; exit 0; fi
         sleep 3
     else
         echo "Select Deny, wait 15 sec."
