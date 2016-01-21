6a59c01ae9f752bdcf84fb6941d6abc19b01972e
knowlet <knowlet3389@gmail.com>
2016-01-21 15:53:49 +0800

    Fix countdown echo

--- countdown.sh
+++ countdown.sh
@@ -1,21 +1,13 @@
-function myEcho()
-{
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        echo -e $1
-    else
-        echo $1
-    fi
-}
 function countdown()
 {
 	if [ $(($1-$(date "+%s"))) -lt 0 ]; then
 		echo "Time Passed!!"
 	else
 		while [[ $(($1-$(date "+%s"))) -ge 0 ]]; do
-			myEcho "\r$(($1-$(date "+%s")))\c"
+			echo -e "\r$(($1-$(date "+%s")))\c"
 			sleep 1
 		done
-		myEcho "\rTime UP!"
+		echo -e "\rTime UP!"
 	fi
 }
 # Usage: countdown 1407295200
